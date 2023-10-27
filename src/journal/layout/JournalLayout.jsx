f3c9cfebc4304ef9a001096d0bf6215944ca369f
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2023-10-26 19:11:43 -0500

    [Add] Prettier styles changes

--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,27 +1,21 @@
-import { Toolbar } from '@mui/material';
-import { Box } from '@mui/system'
-import { NavBar, SideBar } from '../components';
-
+import { Toolbar } from "@mui/material";
+import { Box } from "@mui/system";
+import { NavBar, SideBar } from "../components";
 
 const drawerWidth = 280;
 
 export const JournalLayout = ({ children }) => {
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
+      <NavBar drawerWidth={drawerWidth} />
 
-        <NavBar drawerWidth={ drawerWidth } />
+      <SideBar drawerWidth={drawerWidth} />
 
-        <SideBar drawerWidth={ drawerWidth } />
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
 
-        <Box 
-            component='main'
-            sx={{ flexGrow: 1, p: 3 }}
-        >
-            <Toolbar />
-
-            { children }
-            
-        </Box>
+        {children}
+      </Box>
     </Box>
-  )
-}
+  );
+};
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-import { JournalApp } from './JournalApp';
-import './styles.css';
+import { JournalApp } from "./JournalApp";
+import "./styles.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <JournalApp />
     </BrowserRouter>
   </React.StrictMode>
-)
+);
