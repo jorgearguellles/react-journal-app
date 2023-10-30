a615a56950f50f20683bb3ead7d2f63f0f3d8901
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2023-10-30 17:56:29 -0500

    [Add] Dispatch Auth Actions

--- src/store/auth/thunks.js
+++ src/store/auth/thunks.js
@@ -1,16 +1,18 @@
 import { signInWithGoogle } from "../../firebase/providers";
-import { checkingCredentials } from "./authSlice";
+import { checkingCredentials, login, logout } from "./";
 
-export const checkingAuthentication = (email, password) => {
+export const checkingAuthentication = () => {
   return async (dispatch) => {
     dispatch(checkingCredentials());
   };
 };
 
-export const startGoogleSignIn = (email, password) => {
+export const startGoogleSignIn = () => {
   return async (dispatch) => {
     dispatch(checkingCredentials());
+
     const result = await signInWithGoogle();
-    console.log({ result });
+    if (!result.ok) return dispatch(logout(result.errorMessage));
+    dispatch(login(result));
   };
 };
