b9a00dacc3a28ea019febb3bbb7b8cf67e853d8b
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2023-10-30 15:54:34 -0500

    [Add] Google SignIn by Firebase

---
+++ src/firebase/providers.js
@@ -0,0 +1,21 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { FirebaseAuth } from "./config";
+
+const googleProvider = new GoogleAuthProvider();
+
+export const signInWithGoogle = async () => {
+  try {
+    const result = await signInWithPopup(FirebaseAuth, googleProvider);
+    const { displayName, email, photoURL, uid } = result.user;
+    return {
+      ok: true,
+      displayName,
+      email,
+      uid,
+      photoURL,
+    };
+  } catch (error) {
+    const { errorMessage, errorCode } = error;
+    return { ok: false, errorCode, errorMessage };
+  }
+};
