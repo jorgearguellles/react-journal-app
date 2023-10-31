910980675fca0b3faa2848332b61e4be7b5fbdc1
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2023-10-30 19:51:12 -0500

    [Add] Handler form by custom hook useForm()

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,83 +1,115 @@
+import { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
 import { Button, Grid, Link, TextField, Typography } from "@mui/material";
-import { Google } from "@mui/icons-material";
+
 import { AuthLayout } from "../layout/AuthLayout";
 import { useForm } from "../../hooks";
 
 const formDataByDefault = {
-  email: "jorge@google.com",
-  password: "123456",
-  displayName: "Jorge Arias",
+    email: "jorge@google.com",
+    password: "123456",
+    displayName: "Jorge Arias",
+};
+
+const formValidations = {
+    email: [(value) => value.includes("@"), "Email must had @"],
+    password: [
+        (value) => value.length >= 6,
+        "Password must longer than 6 characters",
+    ],
+    displayName: [
+        (value) => value.length >= 1,
+        "DisplayName must longer than 1 character",
+    ],
 };
 
 export const RegisterPage = () => {
-  const { displayName, email, password, onInputChange, formState } =
-    useForm(formDataByDefault);
+    const [formSubmitted, setFormSubmitted] = useState(false);
+    const {
+        displayName,
+        email,
+        password,
+        onInputChange,
+        formState,
+        displayNameValid,
+        emailValid,
+        passwordValid,
+        isFormValid,
+    } = useForm(formDataByDefault, formValidations);
 
-  const onSubmit = (event) => {
-    event.preventDefault();
-    console.log("formState:", formState);
-  };
+    const onSubmit = (event) => {
+        event.preventDefault();
+        setFormSubmitted(true);
+    };
 
-  return (
-    <AuthLayout title="Create account">
-      <form onSubmit={onSubmit}>
-        <Grid container>
-          <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField
-              label="Full name"
-              type="text"
-              placeholder="Full name"
-              fullWidth
-              name="displayName"
-              value={displayName}
-              onChange={onInputChange}
-              required
-            />
-          </Grid>
+    return (
+        <AuthLayout title="Create account">
+            <form onSubmit={onSubmit}>
+                <Grid container>
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField
+                            label="Full name"
+                            type="text"
+                            placeholder="Full name"
+                            fullWidth
+                            name="displayName"
+                            value={displayName}
+                            onChange={onInputChange}
+                            error={!!displayNameValid && formSubmitted}
+                            helperText={displayNameValid}
+                        />
+                    </Grid>
 
-          <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField
-              label="Email"
-              type="email"
-              placeholder="correo@google.com"
-              fullWidth
-              name="email"
-              value={email}
-              onChange={onInputChange}
-              required
-            />
-          </Grid>
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField
+                            label="Email"
+                            type="email"
+                            placeholder="correo@google.com"
+                            fullWidth
+                            name="email"
+                            value={email}
+                            onChange={onInputChange}
+                            error={!!emailValid && formSubmitted}
+                            helperText={emailValid}
+                        />
+                    </Grid>
 
-          <Grid item xs={12} sx={{ mt: 2 }}>
-            <TextField
-              label="Password"
-              type="password"
-              placeholder="Password"
-              fullWidth
-              name="password"
-              value={password}
-              onChange={onInputChange}
-              required
-            />
-          </Grid>
+                    <Grid item xs={12} sx={{ mt: 2 }}>
+                        <TextField
+                            label="Password"
+                            type="password"
+                            placeholder="Password"
+                            fullWidth
+                            name="password"
+                            value={password}
+                            onChange={onInputChange}
+                            error={!!passwordValid && formSubmitted}
+                            helperText={passwordValid}
+                        />
+                    </Grid>
 
-          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
-            <Grid item xs={12}>
-              <Button variant="contained" fullWidth type="submit">
-                Create account
-              </Button>
-            </Grid>
-          </Grid>
+                    <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+                        <Grid item xs={12}>
+                            <Button variant="contained" fullWidth type="submit">
+                                Create account
+                            </Button>
+                        </Grid>
+                    </Grid>
 
-          <Grid container direction="row" justifyContent="end">
-            <Typography sx={{ mr: 1 }}>¿Already had account?</Typography>
-            <Link component={RouterLink} color="inherit" to="/auth/login">
-              Sign In
-            </Link>
-          </Grid>
-        </Grid>
-      </form>
-    </AuthLayout>
-  );
+                    <Grid container direction="row" justifyContent="end">
+                        <Typography sx={{ mr: 1 }}>
+                            ¿Already had account?
+                        </Typography>
+                        <Link
+                            component={RouterLink}
+                            color="inherit"
+                            to="/auth/login"
+                        >
+                            Sign In
+                        </Link>
+                    </Grid>
+                </Grid>
+            </form>
+        </AuthLayout>
+    );
 };
