04aaa1f80d320c1517d9594720581aae73653199
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2023-11-04 11:36:11 -0500

    [Add] Auth user data persistent and creeate custom hook to check auth status

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,23 +1,23 @@
-import { useSelector } from "react-redux";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
 import { CheckingAuth } from "../ui";
+import { useCheckAuth } from "../hooks";
 
 export const AppRouter = () => {
-    const { status } = useSelector((state) => state.auth);
-    if (status === "checking") {
-        return <CheckingAuth />;
-    }
+    const { status } = useCheckAuth();
+
+    if (status === "checking") return <CheckingAuth />;
 
     return (
         <Routes>
-            {/* Login y Registro */}
-            <Route path="/auth/*" element={<AuthRoutes />} />
-
-            {/* JournalApp */}
-            <Route path="/*" element={<JournalRoutes />} />
+            {status === "authenticated" ? (
+                <Route path="/*" element={<JournalRoutes />} />
+            ) : (
+                <Route path="/auth/*" element={<AuthRoutes />} />
+            )}
+            <Route path="/*" element={<Navigate to="./auth/login" />} />
         </Routes>
     );
 };
