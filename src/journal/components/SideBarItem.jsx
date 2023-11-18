39b142d5b9a7b24d5f67b0d84e40d00bf34d7bbc
Jorge Arias Argüelles <jorgeariasarguelles@gmail.com>
2023-11-17 21:05:21 -0500

    [Add] Active note logic

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -1,4 +1,6 @@
 import React, { useMemo } from "react";
+import { useDispatch } from "react-redux";
+
 import {
     Grid,
     ListItem,
@@ -7,15 +9,22 @@ import {
     ListItemText,
 } from "@mui/material";
 import { TurnedInNot } from "@mui/icons-material";
+import { setActiveNote } from "../../store/journal/journalSlice";
 
-export const SideBarItem = ({ id, body, title = "" }) => {
+export const SideBarItem = ({ id, body, title = "", date, imageUrls = [] }) => {
+    const dispatch = useDispatch();
+
+    const onClickNote = () => {
+        dispatch(setActiveNote({ id, body, title, date }));
+    };
+
     const handleLongTitle = useMemo(() => {
         return title.length > 17 ? title.substring(0, 17) + "..." : title;
     }, [title]);
 
     return (
         <ListItem key={id} disablePadding>
-            <ListItemButton>
+            <ListItemButton onClick={onClickNote}>
                 <ListItemIcon>
                     <TurnedInNot />
                 </ListItemIcon>
